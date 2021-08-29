5f679d797de8b07acd9f556d4280bf0886c4eab1
tim1047 <tim1047@naevr.com>
2021-08-29 16:19:03 +0900

    cors 설정 추가

--- kwjy_account_book/settings.py
+++ kwjy_account_book/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'account_book',
     'rest_framework',
+    'corsheaders',
 ]
 
 # Django REST Framework
@@ -51,6 +52,7 @@ REST_FRAMEWORK = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -132,3 +134,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_ALLOW_ALL = True
