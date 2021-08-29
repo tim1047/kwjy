f957aac78c5ce6b17d87b49f474872588aec04e7
tim1047 <tim1047@naevr.com>
2021-08-29 18:38:41 +0900

    get query paramter 수정

--- account_book/urls.py
+++ account_book/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('account_list', views.AccountList.as_view()),
     path('division_list', views.DivisionList.as_view()),
-    path('category_list/(?P<division_id>\d+)', views.CategoryListByDivisionId.as_view())
+    path('category_list/<int:division_id>', views.CategoryListByDivisionId.as_view())
 ]
