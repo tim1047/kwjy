720e7688bbdecd6411e7e740692e0814bdcf0e66
tim1047 <tim1047@naver.com>
2021-08-30 12:35:11 +0000

    main_list 쿼리수정

--- account_book/sql/get_main_list.sql
+++ account_book/sql/get_main_list.sql
@@ -1,39 +1,45 @@
-select	z.account_id
-	,	z.account_dt
-	,	z.division_nm
-	,	z.member_nm
-	,	z.payment_nm
-	,	z.category_nm
-	,	cd.category_seq_nm
-	,	z.price
-	,	z.remark
-	,	z.impulse_yn
+select	(ROW_NUMBER() OVER()) AS account_id
+	,	zz.*
 from	(
-			select	a.account_id
-				,	a.account_dt
-				,	d.division_nm
-				,	am.member_nm
-				,	p.payment_nm
-				,	c.category_nm
-				,	a.price
-				,	a.remark
-				,	a.impulse_yn
-				,	a.category_id
-				,	a.category_seq
-			from	account			a
-				,	division		d
-				,	account_member	am
-				,	payment			p
-				,	category		c
+			select	z.account_id as seq
+				,	z.account_dt
+				,	z.division_nm
+				,	z.member_nm
+				,	z.payment_nm
+				,	z.category_nm
+				,	cd.category_seq_nm
+				,	z.price
+				,	z.remark
+				,	z.impulse_yn
+			from	(
+						select	a.account_id
+							,	a.account_dt
+							,	d.division_nm
+							,	am.member_nm
+							,	p.payment_nm
+							,	c.category_nm
+							,	a.price
+							,	a.remark
+							,	a.impulse_yn
+							,	a.category_id
+							,	a.category_seq
+						from	account			a
+							,	division		d
+							,	account_member	am
+							,	payment			p
+							,	category		c
+						where	1=1
+						and		a.division_id 	= d.division_id
+						and		a.member_id   	= am.member_id
+						and		a.payment_id  	= p.payment_id
+						and		a.category_id 	= c.category_id
+					) z
+					left outer join category_dtl	cd
+					on 		z.category_id 	= cd.category_id
+					and		z.category_seq 	= cd.category_seq
 			where	1=1
-			and		a.division_id 	= d.division_id
-			and		a.member_id   	= am.member_id
-			and		a.payment_id  	= p.payment_id
-			and		a.category_id 	= c.category_id
-		) z
-		left outer join category_dtl	cd
-		on 		z.category_id 	= cd.category_id
-		and		z.category_seq 	= cd.category_seq
+			order by z.account_dt
+				,	 z.account_id
+		) zz
 where	1=1
-order by z.account_id
 ;
