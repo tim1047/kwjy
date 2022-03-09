6a01158334d9455a6d6ada37ac8ba40e6bab0597
tim1047 <tim1047@naevr.com>
2022-03-09 16:13:39 +0900

    order by 수정

--- account_book/sql/get_division_sum.sql
+++ account_book/sql/get_division_sum.sql
@@ -1,41 +1,45 @@
-select 	z.division_id
-	,	(
-			select	dd.division_nm
-			from	division	dd
-			where	1=1
-			and		dd.division_id = z.division_id
-		) as division_nm
-	,	sum(z.sum_price)  as total_sum_price
+select	z.**
 from	(
-			select	y.category_id
-				,	y.division_id
-				,	coalesce(sum(x.sum_price), 0)	as sum_price
+			select 	z.division_id
+				,	(
+						select	dd.division_nm
+						from	division	dd
+						where	1=1
+						and		dd.division_id = z.division_id
+					) as division_nm
+				,	sum(z.sum_price)  as total_sum_price
 			from	(
-						select	c.category_id
-							  , dcm.division_id
-						from	category	c
-							,	division_category_mpng	dcm
+						select	y.category_id
+							,	y.division_id
+							,	coalesce(sum(x.sum_price), 0)	as sum_price
+						from	(
+									select	c.category_id
+										, dcm.division_id
+									from	category	c
+										,	division_category_mpng	dcm
+									where	1=1
+									and		c.category_id	= dcm.category_id 
+								) y
+						left outer join 
+								(
+									select	sum(a.price)	   as sum_price
+										,	max(a.division_id) as division_id
+										,	a.category_id
+									from	account			a
+									where	1=1
+									and		a.account_dt between %(strt_dt)s and %(end_dt)s
+									group by a.category_id
+										, a.category_seq
+								) x
+						on		y.category_id = x.category_id
+						and		y.division_id = x.division_id
 						where	1=1
-						and		c.category_id	= dcm.category_id 
-					) y
-			left outer join 
-					(
-						select	sum(a.price)	   as sum_price
-							,	max(a.division_id) as division_id
-							,	a.category_id
-						from	account			a
-						where	1=1
-						and		a.account_dt between %(strt_dt)s and %(end_dt)s
-						group by a.category_id
-							   , a.category_seq
-					) x
-			on		y.category_id = x.category_id
-			and		y.division_id = x.division_id
-			where	1=1
-			group by y.category_id
-				, 	 y.division_id
-		) z
-where	1=1		
-group by z.division_id
-order by z.sum_price desc
+						group by y.category_id
+							, 	 y.division_id
+					) z
+			where	1=1		
+			group by z.division_id
+		) zz
+where	1=1
+order by zz.sum_price desc
 ;
--- account_book/sql/get_member_sum.sql
+++ account_book/sql/get_member_sum.sql
@@ -1,14 +1,19 @@
-select	sum(a.price)	as sum_price
-	,	a.member_id
-	,	(
-			select	member_nm
-			from	account_member	am
+
+select	b.*
+from	(
+			select	sum(a.price)	as sum_price
+				,	a.member_id
+				,	(
+						select	member_nm
+						from	account_member	am
+						where	1=1
+						and		am.member_id = a.member_id
+					)	as member_nm
+			from	account			a
 			where	1=1
-			and		am.member_id = a.member_id
-		)	as member_nm
-from	account			a
+			and		a.account_dt between %(strt_dt)s and %(end_dt)s
+			and		a.division_id = '3'
+			group by a.member_id
+		) b
 where	1=1
-and		a.account_dt between %(strt_dt)s and %(end_dt)s
-and		a.division_id = '3'
-group by a.member_id
-order by a.sum_price desc
+order by b.sum_price desc
