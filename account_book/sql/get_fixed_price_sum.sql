27d0c77ea8655e4dbb4218111ce21f1ce79299b8
tim1047 <tim1047@naver.com>
2022-03-13 01:22:04 +0900

    고정지출 비용 수정

--- account_book/sql/get_fixed_price_sum.sql
+++ account_book/sql/get_fixed_price_sum.sql
@@ -1,27 +1,9 @@
-select	cd.category_id
-	,	cd.category_seq
-	,	cd.category_seq_nm
-	,	COALESCE(a.sum_price, 0) as sum_price
-from	category_dtl	cd
-left outer join
-		(
-			select	a.category_id
-				  , a.category_seq
-				  ,	sum(a.price)	as sum_price
-			from	account			a
-				,	category_dtl 	cd
-			where	1=1
-			and		a.category_id = cd.category_id
-			and		a.category_seq = cd.category_seq
-			and		a.account_dt between %(strt_dt)s and %(end_dt)s
-			and		cd.fixed_price_yn = 'Y'
-			group by a.category_id
-				   , a.category_seq
-		) a
-on		cd.category_id = a.category_id
-and		cd.category_seq = a.category_seq
+select	sum(a.price) 	as sum_price
+from	account			a
+	,	category_dtl	cd
 where	1=1
+and		a.category_id = cd.category_id
+and		a.category_seq = cd.category_seq
+and		a.account_dt between %(strt_dt)s and %(end_dt)s
 and		cd.fixed_price_yn = 'Y'
-order by cd.category_id
-	,	cd.category_seq
 ;
