f1a25310a8747ecbf3b4ebec81384aba6a60d5a2
tim1047 <tim1047@naver.com>
2022-03-13 00:48:06 +0900

    고젖 지출 비용 조회 수정

--- account_book/sql/get_fixed_price_sum.sql
+++ account_book/sql/get_fixed_price_sum.sql
@@ -1,13 +1,25 @@
-select	a.category_seq
+select	cd.category_id
+	,	cd.category_seq
 	,	cd.category_seq_nm
-	,	sum(a.price)	as sum_price
-from	account			a
-	,	category_dtl	cd
+	,	a.sum_price
+from	category_dtl	cd
+left outer join
+		(
+			select	a.category_id
+				  , a.category_seq
+				  ,	sum(a.price)	as sum_price
+			from	account			a
+				,	category_dtl 	cd
+			where	1=1
+			and		a.category_id = cd.category_id
+			and		a.category_seq = cd.category_seq
+			and		a.account_dt between %(strt_dt)s and %(end_dt)s
+			and		cd.fixed_price_yn = 'Y'
+			group by a.category_id
+				   , a.category_seq
+		) a
+on		cd.category_id = a.category_id
+and		cd.category_seq = a.category_seq
 where	1=1
-and		a.category_id = cd.category_id
-and		a.category_seq = cd.category_seq
-and		a.account_dt between %(strt_dt)s and %(end_dt)s
 and		cd.fixed_price_yn = 'Y'
-group by a.category_seq
-	   , cd.category_seq_nm
 ;
