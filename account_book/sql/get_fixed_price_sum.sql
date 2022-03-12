261704cc80bc1b92d47bd9f6b1b51504bec4b4f4
tim1047 <tim1047@naver.com>
2022-03-12 14:10:19 +0900

    고정지출비용 수정

--- account_book/sql/get_fixed_price_sum.sql
+++ account_book/sql/get_fixed_price_sum.sql
@@ -1,21 +1,9 @@
-select	x.week
-	,	sum(x.price) 		as sum_price
-from	(
-			select	CASE 
-						WHEN EXTRACT(DAY FROM  to_date(a.account_dt, 'YYYYMMDD')::TIMESTAMP)::INTEGER = 1
-							THEN 1 
-						ELSE extract(week from to_date(a.account_dt, 'YYYYMMDD'):: TIMESTAMP)::integer - extract(week from ( date_trunc('month', to_date(a.account_dt, 'YYYYMMDD')::TIMESTAMP) + interval  '1 day' ) )::integer + 1
-					end as week
-				  , a.price
-			from	account			a
-				,	category_dtl	cd
-			where	1=1
-			and		a.category_id = cd.category_id
-			and		a.category_seq = cd.category_seq
-			and		a.account_dt between %(strt_dt)s and %(end_dt)s
-			and		a.division_id = '3'
-			and		cd.fixed_price_yn = 'Y'
-		) x
+select	sum(a.price) 	as sum_price
+from	account			a
+	,	category_dtl	cd
 where	1=1
-group by x.week
+and		a.category_id = cd.category_id
+and		a.category_seq = cd.category_seq
+and		a.account_dt between %(strt_dt)s and %(end_dt)s
+and		cd.fixed_price_yn = 'Y'
 ;
