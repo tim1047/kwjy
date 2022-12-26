fdc2767b8e24f732d508b05becad72ebb9498513
tim1047 <tim1047@naver.com>
2022-12-26 14:07:59 +0900

    배치 시간 수정

--- kwjy_account_book/settings.py
+++ kwjy_account_book/settings.py
@@ -139,5 +139,5 @@ STATIC_URL = '/static/'
 CORS_ORIGIN_ALLOW_ALL = True
 
 CRONJOBS = [
-    ('*/5 * * * *', 'account_book.cron.asset_accum'),
+    ('* */4 * * *', 'account_book.cron.asset_accum'),
 ]
