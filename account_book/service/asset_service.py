83d8d695e0a82a37be21d207da8f205653eb9da1
tim1047 <tim1047@naver.com>
2026-01-30 13:53:56 +0900

    fdr로 변경

--- account_book/service/asset_service.py
+++ account_book/service/asset_service.py
@@ -49,27 +49,27 @@ def get_crypto_price(coin_id, dt):
 
 def get_usd_krw_rate(dt):
     dt = datetime.datetime.strftime(datetime.datetime.strptime(dt, '%Y%m%d'), '%Y-%m-%d')
-    df_usd = yf.download(['USDKRW=X'], start=dt)
-    # df_usd = fdr.DataReader('USD/KRW', dt, dt)
+    # df_usd = yf.download(['USDKRW=X'], start=dt)
+    df_usd = fdr.DataReader('USD/KRW', dt, dt)
 
     if len(df_usd) == 0:
         dt_date = datetime.datetime.strftime(
             datetime.datetime.strptime(dt, '%Y-%m-%d') - datetime.timedelta(days=7), '%Y-%m-%d'
         )
-        df_usd = yf.download(['USDKRW=X'], start=dt_date)
-        # df_usd = fdr.DataReader('USD/KRW', dt_date, dt)
+        # df_usd = yf.download(['USDKRW=X'], start=dt_date)
+        df_usd = fdr.DataReader('USD/KRW', dt_date, dt)
     return int(df_usd['Close'].iloc[-1].item())
 
 
 def get_jpy_krw_rate(dt):
     dt = datetime.datetime.strftime(datetime.datetime.strptime(dt, '%Y%m%d'), '%Y-%m-%d')
-    df_jpy = yf.download(['JPYKRW=X'], start=dt)
-    # df_jpy = fdr.DataReader('JPY/KRW', dt, dt)
+    # df_jpy = yf.download(['JPYKRW=X'], start=dt)
+    df_jpy = fdr.DataReader('JPY/KRW', dt, dt)
 
     if len(df_jpy) == 0:
         dt_date = datetime.datetime.strftime(
             datetime.datetime.strptime(dt, '%Y-%m-%d') - datetime.timedelta(days=7), '%Y-%m-%d'
         )
-        df_jpy = yf.download(['JPYKRW=X'], start=dt_date)
-        # df_jpy = fdr.DataReader('JPY/KRW', dt_date, dt)
+        # df_jpy = yf.download(['JPYKRW=X'], start=dt_date)
+        df_jpy = fdr.DataReader('JPY/KRW', dt_date, dt)
     return float(df_jpy['Close'].iloc[-1].item())
