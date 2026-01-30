b141e0897c3cbb5610f0b0868d90c8fda970bda4
tim1047 <tim1047@naver.com>
2026-01-30 13:15:00 +0900

    numpy 버전 수정

---
+++ ex.py
@@ -0,0 +1,35 @@
+import yfinance as yf
+import datetime
+# import FinanceDataReader as fdr
+
+
+
+def get_usd_krw_rate(dt):
+    dt = datetime.datetime.strftime(datetime.datetime.strptime(dt, '%Y%m%d'), '%Y-%m-%d')
+    df_usd = yf.download(['USDKRW=X'], start=dt)
+    # df_usd = fdr.DataReader('USD/KRW', dt, dt)
+
+    if len(df_usd) == 0:
+        dt_date = datetime.datetime.strftime(
+            datetime.datetime.strptime(dt, '%Y-%m-%d') - datetime.timedelta(days=7), '%Y-%m-%d'
+        )
+        df_usd = yf.download(['USDKRW=X'], start=dt_date)
+        # df_usd = fdr.DataReader('USD/KRW', dt_date, dt)
+    return int(df_usd['Close'].iloc[-1].item())
+
+def get_jpy_krw_rate(dt):
+    dt = datetime.datetime.strftime(datetime.datetime.strptime(dt, '%Y%m%d'), '%Y-%m-%d')
+    df_jpy = yf.download(['JPYKRW=X'], start=dt)
+    # df_jpy = fdr.DataReader('JPY/KRW', dt, dt)
+
+    if len(df_jpy) == 0:
+        dt_date = datetime.datetime.strftime(
+            datetime.datetime.strptime(dt, '%Y-%m-%d') - datetime.timedelta(days=7), '%Y-%m-%d'
+        )
+        df_jpy = yf.download(['JPYKRW=X'], start=dt_date)
+        # df_jpy = fdr.DataReader('JPY/KRW', dt_date, dt)
+    return float(df_jpy['Close'].iloc[-1].item())
+
+
+print(get_usd_krw_rate('20260129'))
+print(get_jpy_krw_rate('20260129'))
